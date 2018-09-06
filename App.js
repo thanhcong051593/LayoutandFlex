2fe73e0105d1e03a1a5afc08f2402917d3564c37
thanhcong93 <thanhcong051593@gmail.com>
2018-09-06 08:19:12 +0700

    update

--- App.js
+++ App.js
@@ -20,30 +20,98 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={styles.wrapper}>
+        <view style={styles.row}>
+            <view style={styles.col}>
+                <Text style={styles.number}>1</Text>
+            </view>
+
+            <view style={styles.col}>
+                <Text style={styles.number}>2</Text>
+                <Text style={styles.text}>ABC</Text>
+            </view>
+
+            <view style={styles.col}>
+                <Text style={styles.number}>3</Text>
+                <Text style={styles.text}>DEF</Text>
+            </view>
+
+        </view>
+
+        <view style={styles.row}>
+            <view style={styles.col}>
+                <Text style={styles.number}>4</Text>
+                <Text style={styles.text}>GHI</Text>
+            </view>
+
+            <view style={styles.col}>
+                <Text style={styles.number}>5</Text>
+                <Text style={styles.text}>JKL</Text>
+            </view>
+
+            <view style={styles.col}>
+                <Text style={styles.number}>6</Text>
+                <Text style={styles.text}>MNO</Text>
+            </view>
+        </view>
+
+        <view style={styles.row}>
+            <view style={styles.col}>
+                <Text style={styles.number}>7</Text>
+                <Text style={styles.text}>PQRS</Text>
+            </view>
+
+            <view style={styles.col}>
+                <Text style={styles.number}>8</Text>
+                <Text style={styles.text}>TUV</Text>
+            </view>
+
+            <view style={styles.col}>
+                <Text style={styles.number}>9</Text>
+                <Text style={styles.text}>WXYZ</Text>
+            </view>
+        </view>
+
+        <view style={styles.row}>
+            <view style={styles.col}>
+
+            </view>
+
+            <view style={styles.col}>
+                <Text style={styles.number}>0</Text>
+            </view>
+
+            <view style={styles.col}>
+                <Text style={styles.text}>X</Text>
+            </view>
+        </view>
+        
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  wrapper: {
+    flex: 1
+  },
+  row: {
+    flex:1,
+    borderBottomColor: 'grey',
+    borderBottomWidth: 1,
+    flexDirection: row
+  },
+  col: {
+    flex:1,
+    borderRightWidth:1,
+    borderRightColor: 'black',
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems: 'center'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  number: {
+    fontSize: 40
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  text: {
+
+  }
 });
